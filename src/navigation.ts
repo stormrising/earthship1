d931a971727130f814e7455543ff84483d970a55
stormrising <rg.stormrising@gmail.com>
2025-11-28 19:36:04 +0530

    Update navigation.ts

--- src/navigation.ts
+++ src/navigation.ts
@@ -141,13 +141,7 @@ export const footerData = {
     { text: 'Terms', href: getPermalink('/terms') },
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
-  socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/arthelokyo/astrowind' },
-  ],
+  socialLinks: <p> Running on green hosting 🌳 </p>
   footNote: `
     © 2025 ANRF· All rights reserved.
   `,
